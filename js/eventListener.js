5d46ed3e642226e3b35189c565ecbef95df2b1e4
Kruhlmann <andreaskruhlmann@gmail.com>
2015-01-14 08:29:10 +0100

    i fix it yes my friend

--- js/eventListener.js
+++ js/eventListener.js
@@ -1,15 +1,42 @@
-var INNER_WIDTH;
-var INNER_HEIGHT;
-
-function onResize(){
-	INNER_WIDTH = window.innerWidth;
-	INNER_HEIGHT = window.innerHeight;
-}
-
 function getWidth(){
-	return INNER_WIDTH;
+	return window.innerWidth;
 }
 
 function getHeight(){
-	return INNER_HEIGHT;
+	return window.innerHeight;
+}
+
+function onResize(){
+    	var w = getWidth();
+    	if(w <= 780){
+			$( window ).scroll(function() {
+	     		if($(window).scrollTop() > 50){
+	       			$('.top').css({'height': '50'}); 
+	     		}else{
+	         		$('.top').css({'height': '420'}); 
+	     		}
+			});
+	        $( window ).scroll(function() {
+		     	if($(window).scrollTop() > 50){
+			    	$('.mag').css({'height': '50'}); 
+			    }else{
+			        $('.mag').css({'height': '420'}); 
+			    }
+			});
+    	}else {
+			$( window ).scroll(function() {
+	     		if($(window).scrollTop() > 50){
+	       			$('.top').css({'height': '100'}); 
+	     		}else{
+	         		$('.top').css({'height': '420'}); 
+	     		}
+			});
+	        $( window ).scroll(function() {
+		     	if($(window).scrollTop() > 50){
+			    	$('.mag').css({'height': '100'}); 
+			    }else{
+			        $('.mag').css({'height': '420'}); 
+			    }
+			});
+    	}
 }
